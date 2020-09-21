fa824e817f9f425d1ca33e3c0a87e2da68397471
Karra, Umesh (uk179v) <uk179v@att.com>
2020-09-21 13:24:17 +0530

    changes done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from "@angular/forms";
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {DemoMaterialModule} from './material-module';
+import {TablePaginationExample} from './table-pagination-example';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TablePaginationExample
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    DemoMaterialModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
