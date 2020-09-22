c2c08e98b37a054f8a05c2eb6184c388a22cbad9
Karra, Umesh (uk179v) <uk179v@att.com>
2020-09-22 20:18:15 +0530

    files changed as per request

--- src/app/table-pagination-example.ts
+++ src/app/table-pagination-example.ts
@@ -1,4 +1,5 @@
-import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
 
@@ -10,12 +11,61 @@ import {MatTableDataSource} from '@angular/material/table';
   styleUrls: ['table-pagination-example.css'],
   templateUrl: 'table-pagination-example.html',
 })
-export class TablePaginationExample implements AfterViewInit {
+export class TablePaginationExample implements AfterViewInit, OnInit {
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
   dataSource = new MatTableDataSource();
   Status="";
   ObjectId="";
   selectedValue="";
+  heroForm;
+  ELEMENT_DATA = [
+    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+    {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+    {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+    {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
+    {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
+    {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
+    {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
+    {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
+    {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
+    {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
+    {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
+    {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
+    {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
+  ];
+  
+  
+  ELEMENT_DATA1 = [
+    {position: 1, name: 'Hydrogen 1', weight: 1.0079, symbol: 'H'},
+    {position: 2, name: 'Helium 1', weight: 4.0026, symbol: 'He'},
+    {position: 3, name: 'Lithium 1', weight: 6.941, symbol: 'Li'},
+    {position: 4, name: 'Beryllium 1', weight: 9.0122, symbol: 'Be'},
+    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+    {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
+    {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
+    {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
+    {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
+    {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
+    {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
+    {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
+    {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
+    {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
+    {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
+    {position: 21, name: 'Calcium111', weight: 40.078, symbol: 'Ca'},
+  ];
+  
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
   ngAfterViewInit() {
@@ -24,13 +74,39 @@ export class TablePaginationExample implements AfterViewInit {
   onclickfuntion(){
     
   }
+  hero = {
+    name:"",
+    alterEgo:"",
+    power:""
+  }
+  ngOnInit(){
+    
+      this.heroForm = new FormGroup({
+        name: new FormControl(this.hero.name, [
+          Validators.required,
+         
+          Validators.pattern("^[0-9]*$")
+        ]),
+        alterEgo: new FormControl(this.hero.alterEgo),
+        power: new FormControl(this.hero.power, Validators.required)
+      });
+      
+  }
   submitvalue(){
+      
       if(this.selectedValue==="objectid"){
-        this.dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA1);
+        var completedata = this.ELEMENT_DATA.map((object,i)=>{
+          if(object.symbol === "S"){
+            object.symbol = "Changed Text"
+          }
+          return object;
+        })
+        console.log(completedata);
+        this.dataSource = new MatTableDataSource(this.ELEMENT_DATA);
         this.dataSource.paginator = this.paginator;
       }
       if(this.selectedValue==="status"){
-        this.dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+        this.dataSource = new MatTableDataSource(this.ELEMENT_DATA);
         this.dataSource.paginator = this.paginator;
       }
     
@@ -38,60 +114,6 @@ export class TablePaginationExample implements AfterViewInit {
     
 }
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-  {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-  {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-  {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-  {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-  {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-  {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-  {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-  {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-  {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
-];
-
-
-const ELEMENT_DATA1: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen 1', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium 1', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium 1', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium 1', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-  {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-  {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-  {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-  {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-  {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-  {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-  {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-  {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-  {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
-  {position: 21, name: 'Calcium111', weight: 40.078, symbol: 'Ca'},
-];
 
 
 /**  Copyright 2020 Google LLC. All Rights Reserved.
